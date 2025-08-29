93e913b53491b250d3512793634df850932d8af2
Andy Braga <bragovo@gmail.com>
2025-08-30 00:44:04 +0300

    Add core helpers (#1)
    * Init cargo
    
    * Add core helpers
    
    * Add CI
    
    * Add fmt

---
+++ src/settings.rs
@@ -0,0 +1,40 @@
+use std::env;
+
+use config::{Config, Environment, File};
+use serde::{Deserialize, de::DeserializeOwned};
+
+use crate::error::Error;
+
+#[derive(Deserialize, Clone)]
+pub struct HttpSettings {
+    pub endpoint: String,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct DBSettings {
+    pub endpoint: String,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct NATSSettings {
+    pub endpoint: String,
+    pub stream: String,
+}
+
+pub trait Settings<T: DeserializeOwned> {
+    fn new() -> Result<T, Error> {
+        let app_dir = if let Ok(dir) = env::var("APP_DIR") {
+            dir
+        } else {
+            ".".into()
+        };
+
+        let config = Config::builder()
+            .add_source(File::with_name(&format!("{}/settings/default", app_dir)))
+            .add_source(File::with_name(&format!("{}/settings/local", app_dir)).required(false))
+            .add_source(Environment::with_prefix("app").separator("_"))
+            .build()?;
+
+        Ok(config.try_deserialize()?)
+    }
+}
