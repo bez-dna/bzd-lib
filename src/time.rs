2165a03ab69d3100b9ebc4a8ca260b0496063c80
Andy Braga <bragovo@gmail.com>
2026-08-09 14:18:22 +0300

    CHNG

--- src/time.rs
+++ src/time.rs
@@ -2,15 +2,25 @@ use chrono::NaiveDateTime;
 use prost_types::Timestamp;
 
 pub trait ToProtoTimestamp {
-    fn to_option_proto(self) -> Option<Timestamp>;
+    fn to_proto(self) -> Timestamp;
 }
 
 impl ToProtoTimestamp for NaiveDateTime {
-    fn to_option_proto(self) -> Option<Timestamp> {
-        Some(Timestamp {
+    fn to_proto(self) -> Timestamp {
+        Timestamp {
             seconds: self.and_utc().timestamp(),
             nanos: 0,
-        })
+        }
+    }
+}
+
+pub trait ToDateTime {
+    fn to_datetime(self) -> Option<DateTime>;
+}
+
+impl ToDateTime for Timestamp {
+    fn to_datetime(self) -> Option<DateTime> {
+        DateTime::from_timestamp_secs(self.seconds)
     }
 }
 
