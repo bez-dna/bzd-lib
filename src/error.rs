acbd0da7565c1bfe11e81bfff8b5b0db3c91bdef
Andy Braga <bragovo@gmail.com>
2026-09-08 23:00:23 +0300

    Arghhh

--- src/error.rs
+++ src/error.rs
@@ -1,48 +1,14 @@
-use tonic::Status;
-
 pub type Error = anyhow::Error;
 
-#[derive(thiserror::Error, Debug)]
-pub enum AppError {
-    #[error("VALIDATION")]
-    Validation,
-    #[error("NOT_FOUND")]
-    NotFound,
-    #[error("FORBIDDEN")]
-    Forbidden,
-
-    #[error("INTERNAL")]
-    Internal,
-}
-
-// TODO: надо разобраться с этим поглууубже
 #[macro_export]
-macro_rules! impl_from_other {
-    ($($err:ty),+ $(,)?) => {
+macro_rules! internal_from {
+    ($target:ty; $($error:ty),+ $(,)?) => {
         $(
-            impl From<$err> for AppError {
-                fn from(_: $err) -> Self {
+            impl From<$error> for $target {
+                fn from(_: $error) -> Self {
                     Self::Internal
                 }
             }
         )+
     };
-}
-
-impl_from_other!(
-    prost::EncodeError,
-    prost::DecodeError,
-    serde_json::Error,
-    uuid::Error,
-);
-
-impl From<AppError> for Status {
-    fn from(error: AppError) -> Self {
-        match error {
-            AppError::Validation => Self::invalid_argument(error.to_string()),
-            AppError::NotFound => Self::not_found(error.to_string()),
-            AppError::Forbidden => Self::permission_denied(error.to_string()),
-            AppError::Internal => Self::internal(error.to_string()),
-        }
-    }
 }
