25a169b7c628ea02121f2e097d9501fcd8b77660
Andy Braga <bragovo@gmail.com>
2026-09-08 16:18:27 +0300

    Add AppError

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,16 @@
 pub type Error = anyhow::Error;
+
+#[derive(thiserror::Error, Debug)]
+pub enum AppError {
+    #[error("VALIDATION")]
+    Validation,
+    #[error("NOT_FOUND")]
+    NotFound,
+    #[error("FORBIDDEN")]
+    Forbidden,
+
+    #[error("INTERNAL")]
+    Internal,
+    #[error("UNREACHABLE")]
+    Unreachable,
+}
